421b83d4c4a13068daa047eb7513dddd96d73232
Harshit <hc.web1992@gmail.com>
2024-04-03 18:59:39 +0530

    Watching Laravel Blade Template - II -- 24.00

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,92 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Watching Laravel Blade Template - II -- 24.00
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
-Route::get('/about', function () {
-    return view('about', [
-        'hello' => 'Hello hi...'
-    ]);
-});
+// Route::get('/', function () {
+//     return view('home');
+// })->name('/');
 
+// Group routes
+
+// Route::prefix('page')->group(function(){
+//     Route::get('/about', function () {
+//         return "<h1>About Page</h1>";
+//     });
+    
+//     Route::get('/post', function () {
+//         return "<h1>Post Page</h1>";
+//     });
+    
+//     Route::get('/post/firstpost', function () {
+//         return "<h1>First Post Page</h1>";
+//     });
+// });
+
+// Route::get('/page/about', function () {
+//     return "<h1>About Page</h1>";
+// });
+
+// Route::get('/page/post', function () {
+//     return "<h1>Post Page</h1>";
+// });
+
+// Route::get('/page/post/firstpost', function () {
+//     return "<h1>First Post Page</h1>";
+// });
+
+
+
+// named routes
+// Route::get('/about', function () {
+//     return view('about');
+// })->name('about');
+
+// Route::get('/post', function () {
+//     return view('post');
+// })->name('post');
+
+// Route::get('/about/{id?}/comment/{commentid?}', function ($id = null, $commentid = null) {
+
+//     if ($id) {
+//         return "<p>Page ID is: ". $id ."</p> <p>Comment ID is: ". $commentid ."</p>";
+//     }else{
+//         return "<p>No ID Found</p>";
+//     }
+    
+// });
+
+
+// Route Constraints
+// Route::get('/about/{id}', function ($id) {
+
+//     if ($id) {
+//         return "<p>Page ID is: ". $id ."</p>";
+//     }else{
+//         return "<p>No ID Found</p>";
+//     }
+    
+// })->whereNumber("id");
+
+
+
+// Route::get('/about/{id}', function ($id) {
+
+//     if ($id) {
+//         return "<p>Page ID is: ". $id ."</p>";
+//     }else{
+//         return "<p>No ID Found</p>";
+//     }
+    
+// })->whereIn("id", ["harsh", "song", "painting"]);
+
+
+
+
+
+//  7th video completed successfully
